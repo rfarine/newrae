2712616d4de036a356460a667ca4228fbbe22776
Rae <info@rae-farine.net>
2014-10-06 20:42:43 -0400

    got navigation scroll effects working

--- js/coffee/navigation.coffee
+++ js/coffee/navigation.coffee
@@ -1,7 +1,25 @@
-$(document).ready ->
-  menu = $(".centered-navigation-menu")
-  menuToggle = $(".centered-navigation-menu-button")
-  $(menuToggle).on "click", (e) ->
-    e.preventDefault()
-    menu.slideToggle ->
-      menu.removeAttr "style" if menu.is(":hidden")
+Navigation =
+  root: $('html', 'body')
+  menu: $('.centered-navigation-menu')
+  menuButton: $('.centered-navigation-menu-button')
+
+  init: ->
+    Navigation.pageAnchors()
+    Navigation.mobileMenu()
+
+  mobileMenu:  ->
+    Navigation.menuButton.on 'click', (e) ->
+      e.preventDefault()
+      Navigation.menu.slideToggle ->
+        Navigation.menu.removeAttr 'style' if Navigation.menu.is(':hidden')
+
+  pageAnchors: ->
+    $('.nav-link a').on 'click', (e) ->
+      $target = $(this)
+      $anchor = $target.attr("href")
+      $("html, body").stop().animate
+        scrollTop: $($anchor).offset().top
+      , 1000, "easeInOutExpo"
+      e.preventDefault()
+
+Navigation.init()
--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,37 @@
 (function() {
-  $(document).ready(function() {
-    var menu, menuToggle;
-    menu = $(".centered-navigation-menu");
-    menuToggle = $(".centered-navigation-menu-button");
-    return $(menuToggle).on("click", function(e) {
-      e.preventDefault();
-      return menu.slideToggle(function() {
-        if (menu.is(":hidden")) {
-          return menu.removeAttr("style");
-        }
+  var Navigation;
+
+  Navigation = {
+    root: $('html', 'body'),
+    menu: $('.centered-navigation-menu'),
+    menuButton: $('.centered-navigation-menu-button'),
+    init: function() {
+      Navigation.pageAnchors();
+      return Navigation.mobileMenu();
+    },
+    mobileMenu: function() {
+      return Navigation.menuButton.on('click', function(e) {
+        e.preventDefault();
+        return Navigation.menu.slideToggle(function() {
+          if (Navigation.menu.is(':hidden')) {
+            return Navigation.menu.removeAttr('style');
+          }
+        });
       });
-    });
-  });
+    },
+    pageAnchors: function() {
+      return $('.nav-link a').on('click', function(e) {
+        var $anchor, $target;
+        $target = $(this);
+        $anchor = $target.attr("href");
+        $("html, body").stop().animate({
+          scrollTop: $($anchor).offset().top
+        }, 1000, "easeInOutExpo");
+        return e.preventDefault();
+      });
+    }
+  };
+
+  Navigation.init();
 
 }).call(this);
